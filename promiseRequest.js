f1ee78930539688505a4511ead4e2deec9857257
kelshardy <14024@coderacademy.edu.au>
2023-09-07 19:48:11 +1000

    Successful data retrieval via promise

--- promiseRequest.js
+++ promiseRequest.js
@@ -7,15 +7,20 @@ function getRandomPokemon(){
     // let result = fetch("https://pokeapi.co/api/v2/pokemon/" + ID).then()
 
     let ID = 25;
-    let result = fetch(API_BASE_URL + ID).then((response) => {
-        console.log(response);
+	let result = fetch(API_BASE_URL + ID).then((response) => {
+		console.log("Response is: " + JSON.stringify(response, null, 4));
 
-        return response;
-    }).catch(error => {
-        console.log(error);
-    });
+		return response.json();
+	})
+	.then(data => {
+		console.log("Data is: " + JSON.stringify(data, null, 4));
 
-    console.log("Result of API request is:" + result);
+		return data;
+	}).catch(error => {
+		console.log(error);
+	});
+
+    // console.log("Result of API request is:" + result);
 
     // let somePromise = new Promise(resolve, reject => {
 
